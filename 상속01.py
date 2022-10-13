5dc44b06bbdffe9e3258567cf84f8d07e067f194
heesun.joo <heesun9112@naver.com>
2022-10-13 15:16:22 +0900

    22.10.13.class 상속

--- 상속01.py
+++ 상속01.py
@@ -7,15 +7,21 @@ class Person:
             self.name, self.phoneNumber))
 
 class Student(Person):
+    #상속받은 메서드를 덮어쓰기(재정의, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모 초기화 메서드 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
-
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(subject:{0}, studentID: {1})".format(
+            self.subject, self.studentID))
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 p.printInfo()
 s.printInfo()
+print(p)
+print(s)
 

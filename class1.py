428390b47ff457f9ce73bd0e505589a1728c0309
heesun.joo <heesun9112@naver.com>
2022-10-13 11:40:00 +0900

    10.13 11시 class연습2

--- class1.py
+++ class1.py
@@ -9,6 +9,17 @@ class Person:
 
 #2) 인스턴스 생성
 p1 = Person()
-
+p2 = Person()
+p1.name = "전우치"
 #3)메서드 호출
 p1.print()
+p2.print()
+
+#동적인 형식(코드가 실행중-런타임)이라 변수 추가 가능
+#p1 인스턴스 title 없으면 class title로 간다
+Person.title="NN"
+print (p1.title)
+print (p2.title)
+print ( Person.title)
+
+
